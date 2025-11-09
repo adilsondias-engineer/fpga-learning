b59145c77a41d8258e5a4cde0d4046d5269bfb22
Adilson Dias <140462990+adilsondias-engineer@users.noreply.github.com>
2025-11-09 18:44:10 +1100

    Code working but system event may leak random packets

--- build.tcl
+++ build.tcl
@@ -12,6 +12,32 @@ if { $argc != 1 } {
 
 set project_dir [lindex $argv 0]
 
+# Auto-increment build version
+set version_file "${project_dir}/build_version.txt"
+if {[file exists $version_file]} {
+    set fp [open $version_file r]
+    set build_version [read $fp]
+    close $fp
+    set build_version [string trim $build_version]
+    if {![string is integer -strict $build_version]} {
+        set build_version 0
+    }
+} else {
+    set build_version 0
+}
+
+# Increment version
+set build_version [expr {$build_version + 1}]
+
+# Write back to file
+set fp [open $version_file w]
+puts $fp $build_version
+close $fp
+
+puts "=========================================="
+puts "BUILD VERSION: $build_version"
+puts "=========================================="
+
 # Find .xpr file in the project directory
 set xpr_files [glob -nocomplain -directory $project_dir *.xpr]
 if { [llength $xpr_files] == 0 } {
@@ -28,6 +54,10 @@ puts "=========================================="
 
 # Open the project
 open_project $project_file
+
+# Set build version as generic parameter for top-level entity
+set_property generic "BUILD_VERSION=$build_version" [current_fileset]
+puts "Setting generic BUILD_VERSION=$build_version"
 
 # Reset runs to ensure clean build
 reset_run synth_1
